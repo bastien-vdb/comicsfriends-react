d625f1af1fba944392e35436b2e24c2e2d8de7c7
bastien-vdb <bastien.deboisrolin@gmail.com>
2022-08-03 16:43:06 +0800

    navbarmobile+menuMobile created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import AboutComicsFriends from './content/aboutComicsFriends/AboutComicsFriends.js';
 import Account from './Account/Account';
 import 'antd/dist/antd.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css';
 
 function App() {
 
   return (
     <div>
+      <div className='navBarMobile'>
+        <div className='mobileLogo'>
+          <span>Comics Friends</span>
+        </div>
+        <i style={{fontSize:'30px', color:'#124265'}} class="bi bi-list"></i>
+      </div>
+      <div className='mobileMenu'>
+        <div className='mobileMask'></div>
+        <div className='menuAppMobile'></div>
+      </div>
+      
       <div className='menuApp'>
         <section>
           <img className='logoMenu' src='logoMenu.png' alt="Logo menu"/>
